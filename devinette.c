cc937468484cb5ed36d7a10f4551236ef0d3ab56
Maissara Ferkous <e42006149@miage-211-013.dmiage.parisnanterre.fr>
2025-11-03 16:51:59 +0100

    Ajout du fichier devinette.c sur la branche AzizTlili

--- devinette.c
+++ devinette.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    printf("Hello World\n");
+    int nombreMystere, nombreEntre, essais = 0;
+    srand(time(NULL));
+    nombreMystere = rand() % 100 + 1; // nombre entre 1 et 100
+    printf("Devine le nombre mystère (entre 1 et 100) :\\n");
+    do {
+        printf("Ta proposition : ");
+        scanf("%d", &nombreEntre);
+        essais++;
+        if (nombreEntre < nombreMystere)
+            printf("C’est plus !\\n");
+        else if (nombreEntre > nombreMystere)
+            printf("C’est moins !\\n");
+        else
+            printf("Bravo ! Trouvé en %d essais.\\n", essais);
+    } while (nombreEntre != nombreMystere);
     return 0;
 }
